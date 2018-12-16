67a5bd2bee2cad397afd7f2e9ce9841a0a3556e2
MateusPDK <mateus-pk@outlook.com>
2018-12-16 20:11:52 -0200

    End Screen
    Inici da tela de final de jogo.

---
+++ UnoProject/app/src/main/java/com/androide/bobs/unoproject/end_game.java
@@ -0,0 +1,13 @@
+package com.androide.bobs.unoproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class end_game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_game);
+    }
+}
